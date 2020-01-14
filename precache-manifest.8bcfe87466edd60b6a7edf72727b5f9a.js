92d782b2118989d744307ffdcc8294ad7c76e753
YishengSun <yisheng4@illinois.edu>
2020-01-13 21:22:22 -0600

    Updates

---
+++ precache-manifest.8bcfe87466edd60b6a7edf72727b5f9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9bb6a951d9536f6098c58badfd66ce7",
+    "url": "/Roll-Dice/index.html"
+  },
+  {
+    "revision": "2ba0ed1d653c530f062a",
+    "url": "/Roll-Dice/static/css/main.9e19cef2.chunk.css"
+  },
+  {
+    "revision": "4034f56bde0cc95223d5",
+    "url": "/Roll-Dice/static/js/2.4a450a10.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Roll-Dice/static/js/2.4a450a10.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ba0ed1d653c530f062a",
+    "url": "/Roll-Dice/static/js/main.51d44ac0.chunk.js"
+  },
+  {
+    "revision": "8aa1ecf4add4d8f9595b",
+    "url": "/Roll-Dice/static/js/runtime-main.9ac30a82.js"
+  }
+]);
